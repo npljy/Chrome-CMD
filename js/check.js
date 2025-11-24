c55cc849a7fcfa459c9886d9397908c1b84ee6a5
ten-cent <niepengsmile@gmail.com>
2025-11-24 22:15:25 +0800

    ip

--- js/check.js
+++ js/check.js
@@ -3,12 +3,12 @@
     .then(res => res.json())
     .then(data => {
       if (data.isChina) {
-        if (window.location.hostname === 'm3u8.cainiaoblog.cn') {
-          window.location.href = window.location.href.replace('://m3u8.caoniaoblog.cn', '://m3u8.xuehuayu.cn');
+        if (window.location.hostname === 'chrome.cainiaoblog.cn') {
+          window.location.href = window.location.href.replace('://chrome.caoniaoblog.cn', '://chrome.xuehuayu.cn');
         }
       } else {
         if (window.location.hostname === 'm3u8.xuehuayu.cn') {
-          window.location.href = window.location.href.replace('://m3u8.xuehuayu.cn', '://m3u8.caoniaoblog.cn');
+          window.location.href = window.location.href.replace('://chrome.xuehuayu.cn', '://chrome.caoniaoblog.cn');
         }
       }
     });
