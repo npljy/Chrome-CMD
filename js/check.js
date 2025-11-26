6c7881f3d347d414656dc6ae4b17a33d7f0bdbdf
npljy <niepengsmile@gmail.com>
2025-11-26 17:16:28 +0800

    no message

--- js/check.js
+++ js/check.js
@@ -1,8 +1,10 @@
 !(function () {
+  const url = new URL(window.location.href);
+  if (url.searchParams.size) return;
   fetch('https://ipcheck.xuehuayu.cn')
     .then(res => res.json())
     .then(data => {
-      const href = window.location.href;
+      const href = url.toString();
       if (data.isChina) {
         if (window.location.hostname === 'chrome.cainiaoblog.cn') {
           window.location.href = href.replace('://chrome.cainiaoblog.cn', '://chrome.xuehuayu.cn');
