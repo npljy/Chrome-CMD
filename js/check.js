f17bee628fc1794b0ca99e97a9483627fd20fd29
npljy <niepengsmile@gmail.com>
2025-11-26 14:56:49 +0800

    no message

--- js/check.js
+++ js/check.js
@@ -2,13 +2,14 @@
   fetch('https://ipcheck.xuehuayu.cn')
     .then(res => res.json())
     .then(data => {
+      const href = window.location.href;
       if (data.isChina) {
         if (window.location.hostname === 'chrome.cainiaoblog.cn') {
-          window.location.href = window.location.href.replace('://chrome.cainiaoblog.cn', '://chrome.xuehuayu.cn');
+          window.location.href = href.replace('://chrome.cainiaoblog.cn', '://chrome.xuehuayu.cn');
         }
       } else {
         if (window.location.hostname === 'm3u8.xuehuayu.cn') {
-          window.location.href = window.location.href.replace('://chrome.xuehuayu.cn', '://chrome.cainiaoblog.cn');
+          window.location.href = href.replace('://chrome.xuehuayu.cn', '://chrome.cainiaoblog.cn');
         }
       }
     });
