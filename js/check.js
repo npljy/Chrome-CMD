18ba29e8bfb7e4c5db1a8b4983fbb53574a0dd5d
npljy <niepengsmile@gmail.com>
2025-11-27 15:18:21 +0800

    no message

--- js/check.js
+++ js/check.js
@@ -1,22 +1,25 @@
-!(function () {
-  const url = new URL(window.location.href);
-  if (url.searchParams.get('url')) return;
-  fetch('https://ipcheck.xuehuayu.cn')
-    .then(res => res.json())
-    .then(data => {
-      const href = url.toString();
-      const btn = document.getElementById('area');
-      if (data.isChina) {
-        if (window.location.hostname === 'm3u8.cainiaoblog.cn') {
-          btn.innerHTML = '中国站';
-          btn.href = href.replace('://chrome.cainiaoblog.cn', '://chrome.xuehuayu.cn');
-        }
-      } else {
-        if (window.location.hostname === 'm3u8.xuehuayu.cn') {
-          btn.innerHTML = '国际站';
-          btn.href = href.replace('://chrome.xuehuayu.cn', '://chrome.cainiaoblog.cn');
-        }
-      }
-    });
-})()
 
+!(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    const url = new URL(window.location.href);
+    if (url.searchParams.get('url')) return;
+    fetch('https://ipcheck.xuehuayu.cn')
+      .then(res => res.json())
+      .then(data => {
+        const btn = document.getElementById('area');
+        if (!btn) return;
+        const href = url.toString();
+        if (data.isChina) {
+          if (window.location.hostname === 'm3u8.cainiaoblog.cn') {
+            btn.innerHTML = '中国站';
+            btn.href = href.replace('://m3u8.cainiaoblog.cn', '://m3u8.xuehuayu.cn');
+          }
+        } else {
+          if (window.location.hostname === 'm3u8.xuehuayu.cn') {
+            btn.innerHTML = '国际站';
+            btn.href = href.replace('://m3u8.xuehuayu.cn', '://m3u8.cainiaoblog.cn');
+          }
+        }
+      });
+  })
+})();
