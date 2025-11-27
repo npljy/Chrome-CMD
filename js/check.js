97da23d01e2ff96c3bbd4509bea10d855ca2f69c
npljy <niepengsmile@gmail.com>
2025-11-27 15:37:11 +0800

    no message

--- js/check.js
+++ js/check.js
@@ -1,13 +1,13 @@
 
 !(function () {
   document.addEventListener('DOMContentLoaded', function () {
-    const url = new URL(window.location.href);
+    const btn = document.getElementById('area');
+    if (!btn) return;
 
     fetch('https://ipcheck.xuehuayu.cn')
       .then(res => res.json())
       .then(data => {
-        const btn = document.getElementById('area');
-        if (!btn) return;
+        const url = new URL(window.location.href);
         const href = url.toString();
         if (data.isChina) {
           if (window.location.hostname === 'm3u8.cainiaoblog.cn') {
