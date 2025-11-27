be15742fa56f6e41032e0051191404eb69e01e1c
npljy <niepengsmile@gmail.com>
2025-11-27 15:43:08 +0800

    no message

--- js/check.js
+++ js/check.js
@@ -10,12 +10,12 @@
         const url = new URL(window.location.href);
         const href = url.toString();
         if (data.isChina) {
-          if (window.location.hostname === 'm3u8.cainiaoblog.cn') {
+          if (url.hostname === 'm3u8.cainiaoblog.cn') {
             btn.innerHTML = '中国站';
             btn.href = href.replace('://m3u8.cainiaoblog.cn', '://m3u8.xuehuayu.cn');
           }
         } else {
-          if (window.location.hostname === 'm3u8.xuehuayu.cn') {
+          if (url.hostname === 'm3u8.xuehuayu.cn') {
             btn.innerHTML = '国际站';
             btn.href = href.replace('://m3u8.xuehuayu.cn', '://m3u8.cainiaoblog.cn');
           }
